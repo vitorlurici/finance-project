0e2eff6c31da5b9fb49dce014f2f757f058a712d
vitorlosing1 <vitorluricii@hotmail.com>
2023-10-04 00:52:47 -0300

    update-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import { BlurLogo,} from './components/img/blur-logo'
 import { LoginIcon } from './components/img/login-icon'
+import { PersonSmartphone } from './components/img/person-smartphone'
 
 function App() {
   return (
@@ -19,14 +20,40 @@ function App() {
       </header>
       <div className="content-container">
         <div className="left-content">
-        <h1 className="h1">Controle de suas finanças na palma da mão</h1>
-        <h2 className="h2">A Finance fornece uma maneira simples de 
-        cuidar do seu dinheiro de forma gratuita.</h2>
+        <h2>Controle de suas finanças na palma da mão</h2>
+        <h4>A Finance fornece uma maneira simples de 
+        cuidar do seu dinheiro de forma gratuita.</h4>
         <button>Inscreva-se</button>
         </div>
         <div className="right-content">
-        🔥
+          <div className="emoji money">
+          💸
+          </div>
+          <div className="emoji fire">
+          🔥
+          </div>
+          <PersonSmartphone/>
+          <div className="emoji face">
+          🤑
+          </div>
+          <div className="emoji rocket">
+          🚀
+          </div>
         </div>
+      </div>
+
+      <div className="info-container">
+        <div className="left-content">
+
+        </div>
+        <div className="right-content">
+          <h2>Não perca tempo</h2>
+          <h1>Conheça nosso Gerenciador Financeiro</h1>
+        </div>
+      </div>
+
+      <div className="bottom-container">
+
       </div>
 
     </main>
